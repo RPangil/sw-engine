eb507ac8f6b524ac991b28718e90f678c70820fb
RPangil <rapangilinan12@gmail.com>
2023-08-31 12:17:47 -0400

    Added search bar component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,15 +1,29 @@
+import useInput from "@/hooks/use-input";
+
+const notEmpty = (value) => value.trim() !== '';
+
 const SearchBar = () =>
 {
+    const {
+        value: serachValue,
+        inputChange: searchInputChange,
+        inputBlur: searchInputBlur,
+        reset: searchInputReset
+    } = useInput(notEmpty);
 
-    const searchHandler = () =>
+    const searchHandler = (event) =>
     {
+        event.preventDefault();
 
+        console.log(process.env.PLANET_URL+serachValue);
+
+        searchInputReset();
     }
 
     return (
-        <form>
-            <input type="search" />
-            <button type="button" onClick={searchHandler}>Search</button>
+        <form onSubmit={searchHandler}>
+            <input type="search" onChange={searchInputChange} onBlur={searchInputBlur} value={serachValue} />
+            <button onClick={searchHandler}>Search</button>
         </form>
     )
 }
