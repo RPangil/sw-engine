93e63a95e4d4e681604bfe9f21c41ec54ee8cd97
RPangil <rapangilinan12@gmail.com>
2023-08-31 13:55:42 -0400

    Added input hook

--- hooks/use-input.js
+++ hooks/use-input.js
@@ -1,9 +1,10 @@
 import { useState } from "react"
 
-const useInput = () =>
+const useInput = (validateValue) =>
 {
     const [enteredValue, setEnteredValue] = useState('');
     const [isTouched, setIsTouched] = useState(false);
+    const valueIsValid = validateValue(enteredValue);
 
     const inputChange = (event) =>
     {
