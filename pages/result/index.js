8b75a2cfe937944a8c50d8c1a6d55cf48c70b8f5
RPangil <rapangilinan12@gmail.com>
2023-09-13 16:36:39 -0400

    Initial page setup

--- pages/result/index.js
+++ pages/result/index.js
@@ -1,6 +1,6 @@
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
-import { Fragment, useEffect } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 
 const PLANETS_URL = 'https://swapi.dev/api/planets/?search=';
 const PEOPLE_URL = 'https://swapi.dev/api/people/?search=';
@@ -11,13 +11,82 @@ const VEHICLES_URL = 'https://swapi.dev/api/vehicles/?search=';
 
 const results = (props) =>
 {
+    const [searchResult, setSearchResult] = useState([])
+
     const params = useSearchParams();
     const type = params.get('type');
     const searchValue = params.get('val');
+    let count = 0;
+    let paramURL = '';
 
-    console.log(params.get('val'));
+    const fetchResult = useCallback(async () =>
+    {
+        switch(type)
+        {
+            case 'people':
+                paramURL = PEOPLE_URL;
+                break;
+            case 'films':
+                paramURL = FILMS_URL;
+                break;
+            case 'species':
+                paramURL = SPECIES_URL;
+                break;
+            case 'starships':
+                paramURL = STARSHIPS_URL;
+                break;
+            case 'vehicles':
+                paramURL = VEHICLES_URL;
+                break;
+            default:
+                paramURL = PLANETS_URL;
+                break;
+        }
 
-    return;
+        try
+        {
+            let res = await fetch(paramURL+searchValue);
+            let data = await res.json();
+            let resultList = [];
+
+            resultList = data.results;
+            count = data.count;
+            // console.log(data.next);
+
+            while(data.next !== null)
+            {
+                console.log(data.next);
+                console.log(result);
+
+                res = await fetch(data.next);
+                data = await res.json();
+
+                resultList.push(...data.results);
+            }
+
+            setSearchResult(resultList);
+            console.log(resultList);
+            
+            //console.log(data.results[0].birth_year);
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+    });
+
+    useEffect(() =>
+    {
+        fetchResult();
+    }, [fetchResult])
+
+    return (
+        searchResult.length <= 0 ? <p>No results found</p> : 
+        searchResult.map((data) =>
+        {
+            <h1>{data.name}</h1>
+        })
+    );
 }
 
 export default results;
