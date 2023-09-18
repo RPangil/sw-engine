2554910432bedb411112ab87a895b56c0b79a736
RPangil <rapangilinan12@gmail.com>
2023-09-18 16:36:07 -0400

    Added base nav structure

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import styles from './Navigation.module.scss';
+
+const Navigation = () =>
+{
+    return (
+        <nav className={styles.nav}>
+
+        </nav>
+    );
+}
+
+export default Navigation;
