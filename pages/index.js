89d09aee95c58b831c2478fb40a19d4daab23c7c
RPangil <rapangilinan12@gmail.com>
2023-09-18 16:27:07 -0400

    Updated styles and structure

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,16 @@ import style from './main.module.scss';
 export default function Home() {
     return (
         <>
-        <Head>
-            <title>Star Wars Search Engine</title>
-            <meta name="description" content="A search engine that lets you look for characters, species, vehicles and more" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-            <h1 className={style.appTitle}>SW Engine</h1>
-            <SearchBar />
-        </main>
+            <Head>
+                <title>Star Wars Search Engine</title>
+                <meta name="description" content="A search engine that lets you look for characters, species, vehicles and more" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={style.container}>
+                <h1 className={style.appTitle}>SW Engine</h1>
+                <SearchBar />
+            </div>
         </>
     )
 }
